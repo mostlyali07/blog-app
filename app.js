6587fc2c92f442341cc8eb7d1678721a08e27926
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-10-14 03:11:31 +0500

    update

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ var quill = new Quill('#editor', {
 let btnMain = document.getElementById('btnMain');
 
 
-btnMain.addEventListener("click" , () =>{
+btnMain.addEventListener("click", () => {
     let title = document.getElementById("title").value;
     console.log(title);
-    document.getElementById("title").value="";
+    document.getElementById("title").value = "";
+    let editor = document.getElementById("editor").value;
+    console.log(editor);
+    document.getElementById("editor").value = "";
+    let customFile = document.getElementById("customFile").value;
+    console.log(customFile);
+    document.getElementById("customFile").value = "";
 })
