cd968ab69ef57dcbac2e7d53496b47e8f4fe556a
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-10-14 02:48:27 +0500

    update

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ var quill = new Quill('#editor', {
     theme: 'snow'
 });
 
+let title = document.getElementById('title').value;
+let btnMain = document.getElementById('btnMain');
+
+
+btnMain.addEventListener("click" , =>{
+
+})
+console.log(title);
